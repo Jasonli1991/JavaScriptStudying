d2ab25a7609236cfeb1fa031fd8c68a935e0d8b6
Jasonli1991 <chih.chung.li.1991@gmail>
2023-05-01 01:40:53 +0800

    05010140

---
+++ javascript_text/測試/json.js
@@ -0,0 +1,66 @@
+{
+    "productsData": [
+      {
+        "data-id": "1",
+        "data-name": "商品名稱 1",
+        "data-price": "100",
+        "data-image": "https://shoplineimg.com/58d8e05611535017260004c7/62711ad53847270015097080/800x.jpg?",
+        "info": {
+          "description1": "123",
+          "description2": "456"
+        },
+        "style": {
+          "color1": "black",
+          "color2": "white",
+          "color3": "olive-green"
+        }
+      },
+      {
+        "data-id": "2",
+        "data-name": "商品名稱 2",
+        "data-price": "200",
+        "data-image": "https://shoplineimg.com/58d8e05611535017260004c7/637e0a72316a430013e42c87/800x.jpg?",
+        "info": {
+          "description1": "123",
+          "description2": "456"
+        },
+        "style": {
+          "color1": "black",
+          "color2": "white",
+          "color3": "olive-green"
+        }
+      },
+      {
+        "data-id": "3",
+        "data-name": "商品名稱 3",
+        "data-price": "300",
+        "data-image": "https://shoplineimg.com/58d8e05611535017260004c7/62a9ab986d9587001e288342/800x.jpg?",
+        "info": {
+          "description1": "123",
+          "description2": "456"
+        },
+        "style": {
+          "color1": "black",
+          "color2": "white",
+          "color3": "olive-green"
+        }
+      }
+    ],
+    "customersData": [
+      {
+        "name": "jason",
+        "account": "jason123",
+        "code": "135246"
+      },
+      {
+        "name": "mary",
+        "account": "mary123",
+        "code": "246789"
+      },
+      {
+        "name": "peter",
+        "account": "peter123",
+        "code": "976123"
+      }
+    ]
+  }
